66f375b8c917f6d12a607f74790d9028fded8c35
王楠 <773013686@qq.com>
2017-10-16 01:52:08 +0800

    update gulpfile.js

---
+++ app/js/AbstractApplication.js
@@ -0,0 +1,50 @@
+import "three"
+import 'three/examples/js/controls/OrbitControls'
+
+class AbstractApplication {
+  constructor() {
+    this._camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 1, 1000)
+    this._camera.position.z = 400
+
+    this._scene = new THREE.Scene()
+    this._renderer = new THREE.WebGLRenderer()
+    this._renderer.setPixelRatio(window.devicePixelRatio)
+    this._renderer.setSize(window.innerWidth, window.innerHeight)
+    document.appendChild(this._renderer.domElement)
+
+    this._controls = new THREE.OrbitControls( this._camera, this._renderer.domElement );
+
+    this._timeout_window_resize = null
+    window.addEventListener('resize', this.onWindowResize.bind(this), false)
+  }
+
+  get renderer () {
+    return this._renderer
+  }
+
+  get camera () {
+    return this._camera
+  }
+
+  get scene () {
+    return this._scene
+  }
+
+  onWindowResize () {
+    clearTimeout(this._timeout_window_resize)
+    setTimeout(() => {
+      this._camera.aspect = window.innerWidth / window.innerHeight
+      this._camera.updateProjectionMatrix()
+
+      this._renderer.setSize(window.innerWidth, window.innerHeight)
+    }, this._timeout_window_resize)
+  }
+
+  animate (timestamp) {
+    requestAnimationFrame(this.animate.bind(this))
+    this._renderer.render(this._scene, this._camera)
+  }
+
+}
+
+export default AbstractApplication
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,65 @@
 const gulp = require('gulp');
 
+const del = require('del');
 const eslint = require('gulp-eslint');
+const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
 
-gulp.task('lint', () => {
-    return gulp.src(['src/*.js', '!node_modules/**'])
-        .pipe(eslint('.eslintrc.js'))
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+const paths = {
+  scripts: ['app/**/*.js'],
+  images: 'app/img/**/*'
+};
+
+gulp.task('clean', () => {
+  return del(['build']);
+})
+
+gulp.task('eslint', () => {
+  return gulp.src(['app/*.js', '!node_modules/**'])
+    .pipe(eslint('.eslintrc.js'))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
-gulp.task('default', ['lint'], () => {
+gulp.task('scripts', ['eslint'], () => {
+  return gulp.src(paths.scripts)
+    .pipe(sourcemaps.init())
+      .pipe(babel({
+        presets: ['env']
+      }))
+      .pipe(uglify())
+      .pipe(concat('all.min.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/js'));
+})
+
+// Copy all static images
+gulp.task('images', ['clean'], function() {
+  return gulp.src(paths.images)
+    // Pass in options to the task
+    .pipe(imagemin({optimizationLevel: 5}))
+    .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('js-watch', ['scripts'], (done) => {
+  browserSync.reload();
+  done();
+})
+
+gulp.task('browser-sync', ['scripts'], () => {
+  browserSync.init({
+    server: {
+      baseDir: './app'
+    }
+  });
+
+  gulp.watch('app/*.js', ['js-watch']);
+})
+
+gulp.task('default', ['browser-sync', 'js-watch'], () => {
     console.log('All tasks completed successfully.');
 })
